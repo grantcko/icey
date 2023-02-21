7763413d7bb2e785cbc86b8b055ee829a2b9ff33
Grant Hall <grantclone@gmail.com>
2023-02-21 13:21:37 +0900

    updated rental model

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -2,6 +2,8 @@ class Rental < ApplicationRecord
   belongs_to :user
   belongs_to :jewelry
 
+  enum status: [ :pending, :accepted, :rejected ]
+
   validates :status, presence: true
   validates :start_date, presence: true
   validates :end_date, presence: true
