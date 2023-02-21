29968b368a717d2862de86ff3ef5679dcca1897c
Rungthip Chaothayee <ttann.trc@gmail.com>
2023-02-21 15:57:50 +0900

    finish creating seeds for all database

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,19 +8,37 @@
 puts 'Cleaning the DB..'
 Jewelry.destroy_all
 User.destroy_all
+Rental.destroy_all
 
-u = User.create!(name: "Grant", email: "grant@gmail.com", password: "123123")
- 
-Jewelry.create!(
-  user: u,
-  name: 'Justice Scale Chain',
+grant = User.create!(name: "Grant", email: "grant@gmail.com", password: "123123")
+taka = User.create!(name: "Taka", email: "taka@gmail.com", password: "123123")
+
+puts "... created #{User.count} users."
+
+jew1 = Jewelry.create!(
+  user: grant,
+  name: 'Grant Scale Chain',
   detail: 'Larger than my actual face, this glorious accessory would eventually take on a whole new world',
-  price: 30_000
+  price: 50_000
 )
-Jewelry.create!(user: u, name: 'Grant Scale Chain', detail: 'Larger than my actual face, this glorious accessory would eventually take on a whole new world', price: 50_000)
-Jewelry.create!(user: u, name: 'Anik Scale Earring', detail: 'Larger than my actual face, this glorious accessory would eventually take on a whole new world', price: 10_000)
-Jewelry.create!(user: u, name: 'Sexy Taka Scale Chain', detail: 'Larger than my actual face, this glorious accessory would eventually take on a whole new world', price: 20_000)
-Jewelry.create!(user: u, name: 'Super Sexy Tan Scale Chain', detail: 'Larger than my actual face, this glorious accessory would eventually take on a whole new world', price: 45_000)
-Jewelry.create!(user: u, name: 'Tan sexy than Taka Chain', detail: 'Larger than my actual face, this glorious accessory would evetually take on a whole new world', price: 70_000)
+jew2 = Jewelry.create!(user: grant, name: 'Anik Scale Earring', detail: 'Larger than my actual face, this glorious accessory would eventually take on a whole new world', price: 10_000)
+jew3 = Jewelry.create!(user: grant, name: 'Sexy Taka Scale Chain', detail: 'Larger than my actual face, this glorious accessory would eventually take on a whole new world', price: 20_000)
+jew4 = Jewelry.create!(user: taka, name: 'Super Sexy Tan Scale Chain', detail: 'Larger than my actual face, this glorious accessory would eventually take on a whole new world', price: 45_000)
+jew5 = Jewelry.create!(user: taka, name: 'Tan sexy than Taka Chain', detail: 'Larger than my actual face, this glorious accessory would evetually take on a whole new world', price: 70_000)
 
-puts "... created #{Jewelry.count} jewelry."
+puts "... created #{Jewelry.count} jewelries."
+
+
+Rental.create!(
+  user: grant,
+  jewelry: jew2,
+  status: 2,
+  start_date: Date.today,
+  end_date: Date.today + 20
+)
+Rental.create!(user: grant, jewelry: jew3, status: 1, start_date: Date.today, end_date: Date.today + 4)
+Rental.create!(user: taka, jewelry: jew4, status: 0, start_date: Date.today, end_date: Date.today + 22)
+Rental.create!(user: taka, jewelry: jew5, status: 1, start_date: Date.today, end_date: Date.today + 15)
+Rental.create!(user: taka, jewelry: jew1, status: 0, start_date: Date.today, end_date: Date.today + 10)
+
+puts "... created #{Rental.count} rentals."
