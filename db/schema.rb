a8c041089a58cd4dd21a23ebe3f1d623090e2912
Grant Hall <grantclone@gmail.com>
2023-02-21 12:30:58 +0900

    made jewelry model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_20_090438) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_21_031651) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "jewelries", force: :cascade do |t|
+    t.string "name"
+    t.text "detail"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
