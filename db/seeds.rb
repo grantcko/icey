3fa6596880a45adc960d0f5bd8ce2b432ce884f0
Grant Hall <grantclone@gmail.com>
2023-02-21 15:06:01 +0900

    add-user-reference-to-jewelry

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,20 @@
 #   Character.create(name: "Luke", movie: movies.first)
 puts 'Cleaning the DB..'
 Jewelry.destroy_all
+User.destroy_all
 
-Jewelry.create(name: 'Justice Scale Chain', detail: 'Larger than my actual face, this glorious accessory would eventually take on a whole new world', price: 30_000)
-Jewelry.create(name: 'Grant Scale Chain', detail: 'Larger than my actual face, this glorious accessory would eventually take on a whole new world', price: 50_000)
-Jewelry.create(name: 'Anik Scale Earring', detail: 'Larger than my actual face, this glorious accessory would eventually take on a whole new world', price: 10_000)
-Jewelry.create(name: 'Sexy Taka Scale Chain', detail: 'Larger than my actual face, this glorious accessory would eventually take on a whole new world', price: 20_000)
-Jewelry.create(name: 'Super Sexy Tan Scale Chain', detail: 'Larger than my actual face, this glorious accessory would eventually take on a whole new world', price: 45_000)
-Jewelry.create(name: 'Tan sexy than Taka Chain', detail: 'Larger than my actual face, this glorious accessory would eventually take on a whole new world', price: 70_000)
+u = User.create!(name: "Grant", email: "grant@gmail.com", password: "123123")
+
+Jewelry.create!(
+  user: u,
+  name: 'Justice Scale Chain',
+  detail: 'Larger than my actual face, this glorious accessory would eventually take on a whole new world',
+  price: 30_000
+)
+Jewelry.create!(user: u, name: 'Grant Scale Chain', detail: 'Larger than my actual face, this glorious accessory would eventually take on a whole new world', price: 50_000)
+Jewelry.create!(user: u, name: 'Anik Scale Earring', detail: 'Larger than my actual face, this glorious accessory would eventually take on a whole new world', price: 10_000)
+Jewelry.create!(user: u, name: 'Sexy Taka Scale Chain', detail: 'Larger than my actual face, this glorious accessory would eventually take on a whole new world', price: 20_000)
+Jewelry.create!(user: u, name: 'Super Sexy Tan Scale Chain', detail: 'Larger than my actual face, this glorious accessory would eventually take on a whole new world', price: 45_000)
+Jewelry.create!(user: u, name: 'Tan sexy than Taka Chain', detail: 'Larger than my actual face, this glorious accessory would evetually take on a whole new world', price: 70_000)
 
 puts "... created #{Jewelry.count} jewelry."
