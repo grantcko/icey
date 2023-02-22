94e064f79e91227fc6948fc406f708b55aadf5a9
takaakinakagami <takaaki0120n@gmail.com>
2023-02-22 14:25:02 +0900

    add cloudnary

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
+require "open-uri"
 puts 'Cleaning the DB..'
 Rental.destroy_all
 Jewelry.destroy_all
 User.destroy_all
+
+file = URI.open("https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jpg/1200px-NES-Console-Set.jpg")
+article = Article.new(title: "NES", body: "A great console")
+article.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+article.save
+
 
 grant = User.create!(
   name: "Grant",
