0709f80753d14cc57f35ecbcd80697b3eaf7301a
takaakinakagami <takaaki0120n@gmail.com>
2023-02-24 15:03:25 +0900

    profile pics added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 require "open-uri"
+require 'nokogiri'
 puts 'Cleaning the DB..'
 Rental.destroy_all
 Jewelry.destroy_all
@@ -28,39 +29,68 @@ photo_array = [
 "https://res.cloudinary.com/dn2mnawil/image/upload/v1677032603/Icey%20Bling-Bling/icey007_inamrs.jpg"
 ]
 
-def url_file
+5.times do
+  user = User.create!(
+  name: Faker::Name.name,
+  email: Faker::Internet.safe_email,
+  password: '123123', # needs to be 6 digits,
+  # add any additional attributes you have on your model
+)
+  url = 'https://this-person-does-not-exist.com/en'
+  doc = Nokogiri::HTML(URI.open(url).read)
+  src = doc.search('#avatar').first['src']
+  p photo_url = "https://this-person-does-not-exist.com#{src}"
+  file = URI.open(photo_url)
+  user.photo.attach(io: file, filename: 'user.png', content_type: 'image/png')
 end
+
 taka = User.create!(
   name: "taka",
   email: "taka@gmail.com",
   password: "123123"
   )
+  p photo_url = "https://avatars.githubusercontent.com/u/117798839?v=4"
+  file = URI.open(photo_url)
+  taka.photo.attach(io: file, filename: 'user.png', content_type: 'image/png')
 
 grant = User.create!(
   name: "grant",
   email: "grant@gmail.com",
   password: "123123"
   )
+  p photo_url = "https://avatars.githubusercontent.com/u/121933082?v=4"
+  file = URI.open(photo_url)
+  grant.photo.attach(io: file, filename: 'user.png', content_type: 'image/png')
 
 tan = User.create!(
   name: "tan",
   email: "tan@gmail.com",
   password: "123123"
   )
-
+  p photo_url = "https://avatars.githubusercontent.com/u/121886405?v=4"
+  file = URI.open(photo_url)
+  tan.photo.attach(io: file, filename: 'user.png', content_type: 'image/png')
 anik = User.create!(
   name: "Anik",
   email: "anik@gmail.com",
   password: "123123"
 )
-
+  p photo_url = "https://avatars.githubusercontent.com/u/80834097?v=4"
+  file = URI.open(photo_url)
+  anik.photo.attach(io: file, filename: 'user.png', content_type: 'image/png')
 puts "... created #{User.count} users."
 
 jewelries = ['Watch', 'Gold Watch', 'Diamond Necklace', 'Chain', 'Small Chain', 'Big Chain', 'Ring', 'Necklace', 'Bracelet']
+details = [
+  'BLING BLING to make your life SHINE SHINE',
+  'It is great piece of jewelry that you could wear it to special occation',
+  'It is sooooo shine that make everyone eyes go blind!!'
+]
+prices = ['3000','4000', '5000', '8000', '12000', '20000']
 
 12.times do
   file = URI.open(photo_array.sample)
-  jewelry = Jewelry.new(name: Faker::Cannabis.strain.split.first + " " + jewelries.sample, detail: "A great console", price: 5000)
+  jewelry = Jewelry.new(name: Faker::Cannabis.strain.split.first + " " + jewelries.sample, detail: details.sample, price: prices.sample)
   jewelry.photo.attach(io: file, filename: "nes.jpg", content_type: "image/jpg")
   jewelry.user = grant
   jewelry.save
@@ -70,7 +100,7 @@ puts "... created #{Jewelry.count} jewelries."
 
 6.times do
   file = URI.open(photo_array.sample)
-  jewelry = Jewelry.new(name: Faker::Cannabis.strain.split.first + " " + jewelries.sample, detail: "A great console", price: 5000)
+  jewelry = Jewelry.new(name: Faker::Cannabis.strain.split.first + " " + jewelries.sample, detail: details.sample, price: prices.sample)
   jewelry.photo.attach(io: file, filename: "nes.jpg", content_type: "image/jpg")
   jewelry.user = grant
   jewelry.save
@@ -87,8 +117,8 @@ end
   file = URI.open(photo_array.sample)
   jewelry = Jewelry.new(
     name: "#{Faker::Cannabis.strain.split.first} #{jewelries.sample}",
-     detail: "A great console",
-     price: 5000
+     detail: details.sample,
+     price: prices.sample
   )
   jewelry.photo.attach(io: file, filename: "nes.jpg", content_type: "image/jpg")
   jewelry.user = taka
@@ -106,8 +136,8 @@ end
   file = URI.open(photo_array.sample)
   jewelry = Jewelry.new(
     name: "#{Faker::Cannabis.strain.split.first} #{jewelries.sample}",
-    detail: "A great console",
-    price: 5000
+    detail: details.sample,
+    price: prices.sample
   )
   jewelry.photo.attach(io: file, filename: "nes.jpg", content_type: "image/jpg")
   jewelry.user = anik
@@ -126,8 +156,8 @@ end
 
   jewelry = Jewelry.new(
     name: "#{Faker::Cannabis.strain.split.first} #{jewelries.sample}",
-    detail: "A great console",
-    price: 5000
+    detail: details.sample,
+    price: prices.sample
   )
 
   jewelry.photo.attach(io: file, filename: "nes.jpg", content_type: "image/jpg")
