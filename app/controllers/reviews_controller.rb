5cc2b75702837decdd86bee42fb24f30ab6d0f2d
Rungthip Chaothayee <ttann.trc@gmail.com>
2023-02-24 16:10:47 +0900

    added a review function

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -13,9 +13,25 @@ class ReviewsController < ApplicationController
     authorize @review
 
     if @review.save
-      redirect_to jewelry_path(@jewelry)
+      respond_to do |format|
+        format.html { redirect_to jewelry_path(@jewelry) }
+        format.json do
+          render json: {
+            review_html: render_to_string(partial: 'reviews/review', formats: :html, locals: { review: @review }),
+            form_html: render_to_string(partial: 'reviews/new', formats: :html, locals: { jewelry: @review.jewelry, review: Review.new }),
+          }.to_json
+        end
+      end
     else
-      render "jewelrys/show", status: :unprocessable_entity
+      respond_to do |format|
+        # display the form again
+        format.html { render :new, status: :unprocessable_entity }
+        format.json do
+          render json: {
+            form_html: render_to_string(partial: 'reviews/new', formats: :html, locals: { jewelry: @review.jewelry, review: @review }),
+          }.to_json
+        end
+      end
     end
   end
 
